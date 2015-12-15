d1db77b8bc9c1256e9c0ea8c1a6ce6d869ebe034
mh252 <mh252@localhost>
2015-12-15 08:16:58 +0100

    implemented graphics

--- Memory/src/gui/gui.java
+++ Memory/src/gui/gui.java
@@ -10,6 +10,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -26,6 +28,23 @@ public class gui extends Application{
 	static Button[] field = new Button[16];
 	int flippedCounter=0;
 	
+	static Image[] cardPictures = new Image[9];
+	
+	static{
+		cardPictures[0] = new Image("file:/stud/mh252/git/Semester2.1/Memory/src/gui/Dachsnormal.png");
+		cardPictures[1] = new Image("file:/stud/mh252/git/Semester2.1/Memory/src/gui/loewe.png");
+		cardPictures[2] = new Image("file:/stud/mh252/git/Semester2.1/Memory/src/gui/monkey.png");
+		cardPictures[3] = new Image("file:/stud/mh252/git/Semester2.1/Memory/src/gui/doge.png");
+		
+		cardPictures[4] = new Image("file:/stud/mh252/git/Semester2.1/Memory/src/gui/horse.png");
+		cardPictures[5] = new Image("file:/stud/mh252/git/Semester2.1/Memory/src/gui/yak.png");
+		cardPictures[6] = new Image("file:/stud/mh252/git/Semester2.1/Memory/src/gui/cow.png");
+		cardPictures[7] = new Image("file:/stud/mh252/git/Semester2.1/Memory/src/gui/tiger.png");
+		cardPictures[8] = new Image("file:/stud/mh252/git/Semester2.1/Memory/src/gui/back.png");
+
+	}
+	
+
 	
 	
 	@Override
@@ -44,8 +63,13 @@ public class gui extends Application{
 		
 		//setup buttons
 		
+		
+		
+		
+		
 		for(int i=0; i<field.length; i++){
-			field[i] = new Button(""+(i+1));
+			field[i] = new Button();
+			field[i].setGraphic(new ImageView(cardPictures[8]));
 			field[i].setMaxSize(200D, 200D);
 			field[i].setPrefSize(200D, 200D);
 			field[i].setMinSize(10D, 10D);
@@ -70,7 +94,12 @@ public class gui extends Application{
 						}
 						
 					
-						tmp.setText(grid.getCard(index).getValue());
+						//tmp.setText(grid.getCard(index).getValue());
+						setButtonGraphic(tmp, grid.getCard(index).getValue());
+						//tmp.setGraphic(new ImageView(cardPictures[index]));
+						
+						tmp.setText("");
+						
 						grid.getCard(index).setState(state.VISIBLE);
 						
 						flipped[flippedCounter]=grid.getCard(index);
@@ -105,21 +134,7 @@ public class gui extends Application{
 			});
 
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 
 		//Graphic UI
 		HBox[] rows = new HBox[4];
@@ -166,21 +181,54 @@ public class gui extends Application{
 			
 		if(grid.getCard(i).getState()==MemoryCardInterface.state.VISIBLE){
 			grid.getCard(i).setState(state.UNSOLVED);
-			field[i].setText(""+(i+1));
+			field[i].setGraphic(new ImageView(cardPictures[8]));
 		}
 		}
 
 	}
 	
+	public static void setButtonGraphic(Button btn, String text){
+		
+		switch (text){
+		
+		case "Dachs":
+			btn.setGraphic(new ImageView(cardPictures[0]));
+			
+		break;
+			
+		case "Löwe":
+			btn.setGraphic(new ImageView(cardPictures[1]));
+		break;
+			
+		case "Yak":
+			btn.setGraphic(new ImageView(cardPictures[5]));
+			break;
+		case "Hund":
+			btn.setGraphic(new ImageView(cardPictures[3]));
+			break;
+		case "Affe":
+			btn.setGraphic(new ImageView(cardPictures[2]));
+			break;
+		case "Pferd":
+			btn.setGraphic(new ImageView(cardPictures[4]));
+			break;
+		case "Tiger":
+			btn.setGraphic(new ImageView(cardPictures[7]));
+			break;
+		case "Kuh":
+			btn.setGraphic(new ImageView(cardPictures[6]));
+			break;
+		default:
+			break;
+		}
+		
+		
+	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
-		
-		
-		
-		
-		
-		
+
 		
 		
 	}
